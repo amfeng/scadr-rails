21cabf01e33397d394dda9638988492d7835f6b9
Karl He <karl.he@gmail.com>
2011-01-05 20:15:52 -0800

    updated avro record to work with new jar

--- lib/avro_record.rb
+++ lib/avro_record.rb
@@ -1,24 +1,48 @@
 require File.join(RAILS_ROOT,'lib/piql/piql.jar')
 
-#import Java::EduBerkeleyCsScadsPiql::UserKey
-#import Java::EduBerkeleyCsScadsPiql::UserValue
-import Java::EduBerkeleyCsScadsPiql::ScadrClient
+import Java::EduBerkeleyCsScadsPiqlScadr::ScadrClient
 import Java::EduBerkeleyCsScadsPiql::SimpleExecutor
 import Java::EduBerkeleyCsScadsStorage::TestScalaEngine
 
-$CLIENT = ScadrClient.new(TestScalaEngine.getTestCluster, SimpleExecutor.new, 10)
+$CLIENT = ScadrClient.new(TestScalaEngine.new_scads_cluster(1), SimpleExecutor.new, 10)
+
+class Object
+  # Mainly to convert String to Utf8 without having to check if an Object is a String
+  # Can help if anything besides a String needs a similar process
+  def pickle
+    self
+  end
+end
 
 class String
   def carmelize
     base = self.camelize
     base.slice(0,1).downcase + base.slice(1,base.size)
   end
+  
+  # Convert String to Utf8
+  def pickle
+    Java::OrgApacheAvroUtil.Utf8.new(self)
+  end
 end
 
-# TODO: Can't figure out what te object PIQL queries return is
+# Wrapper around ListBuffer, to make it more like an array
+class ScalaList
+  def initialize
+    @list_buffer = Java::ScalaCollectionMutable::ListBuffer.new
+  end
+  
+  def push(item)
+    @list_buffer.append{ |foo| foo.apply item.pickle }
+  end
+  
+  def to_list
+    @list_buffer
+  end
+end
+
 class AvroRecord
-  attr_accessor :avro_key
-  attr_accessor :avro_value
+  attr_accessor :avro_pair
 
   # TODO: Enforce primary keys; Does PIQL handle this?
   def self.set_primary_key(key)
@@ -30,8 +54,7 @@ class AvroRecord
   def initialize(opts={})
     @is_new_record = true
     begin
-      @avro_key = eval("Java::EduBerkeleyCsScadsPiql::#{self.class.to_s}Key").new
-      @avro_value = eval("Java::EduBerkeleyCsScadsPiql::#{self.class.to_s}Value").new
+      @avro_pair = eval("Java::EduBerkeleyCsScadsPiqlScadr::#{self.class.to_s}").new
     # Catches if the class does not exist
     rescue NameError => e
       raise e, "#{e.message} -- does #{self.class} exist in the schema?"
@@ -60,7 +83,7 @@ class AvroRecord
   # TODO
   def save
     begin
-      $CLIENT.send(self.class.to_s.downcase.pluralize).put(avro_key, avro_value)
+      $CLIENT.send(self.class.to_s.downcase.pluralize).put(avro_pair.key, avro_pair.value)
       @is_new_record = false
       true
     rescue NativeException
@@ -80,35 +103,36 @@ class AvroRecord
   def self.method_missing(method_id, *arguments, &block)
     # Find
     if $CLIENT.respond_to?(method_id)
+      sl = ScalaList.new
+      arguments.each { |arg| sl.push(arg) }
+    
       begin
-        raw_results = $CLIENT.send(method_id, *arguments, &block)
+        raw_results = $CLIENT.send(method_id).apply { sl.to_list }
       rescue Exception => e
         raw_results = []
       end
       results = []
       
       while results.size < raw_results.size do
-        keystore, valuestore = raw_results.apply(results.size)
+        rows = raw_results.apply(results.size)
+        # instance = self.fetch
         
-        instance = self.fetch
-        
-        key_schema = keystore.getSchema.getFields
-        (0...key_schema.size).each do |index|
-          field = key_schema.get(index).name
-          value = keystore.get(index)
-          value = value.to_s if value.is_a?(Java::OrgApacheAvroUtil::Utf8)
-          instance.send(field.underscore+"=", value)
-        end
-
-        value_schema = valuestore.getSchema.getFields
-        (0...value_schema.size).each do |index|
-          field = value_schema.get(index).name
-          value = valuestore.get(index)
-          value = value.to_s if value.is_a?(Java::OrgApacheAvroUtil::Utf8)
-          instance.send(field.underscore+"=", value)
+        tuple = []
+        (0...rows.size).each do |row_index|
+          row = rows.apply(row_index)
+          schema = row.schema
+          fields = schema.fields
+          instance = schema.name.constantize.new
+          (0...fields.size).each do |field_index|
+            field = fields.get(field_index).name
+            value = row.send(field)
+            value = value.to_s if value.is_a?(Java::OrgApacheAvroUtil::Utf8)
+            instance.send(field.underscore+"=", value)
+          end
+          tuple.push(instance)
         end
         
-        results.push instance
+        results.push tuple
       end
       results
     else
@@ -119,7 +143,7 @@ class AvroRecord
   def respond_to?(method_id, include_private=false)
     method_id.to_s =~ /^(\w+)(=)?/
     column_name = $1.carmelize
-    if avro_key.getSchema.getField(column_name) || avro_value.getSchema.getField(column_name)
+    if avro_pair.schema.get_field(column_name)
       true
     else
       super
@@ -133,11 +157,11 @@ class AvroRecord
     column_name = $1.carmelize
     method_name = $2 ? (column_name + "_$eq") : column_name
     # Key
-    if avro_key.getSchema.getField(column_name)
-      avro_key.send(method_name, *arguments, &block)
+    if avro_pair.schema.get_field(column_name)
+      avro_pair.send(method_name, *arguments, &block)
     # Value
-    elsif avro_value.getSchema.getField(column_name)
-      avro_value.send(method_name, *arguments, &block)
+    elsif avro_value.schema.get_field(column_name)
+      avro_pair.send(method_name, *arguments, &block)
     # Default
     else
       super
