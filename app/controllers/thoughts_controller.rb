5852f2b3d2416e95547561c0cf7a3e516f32576f
Karl He <karl.he@gmail.com>
2011-01-26 06:10:35 -0800

    added jquery, really crappy ajax refreshing of thoughts

--- app/controllers/thoughts_controller.rb
+++ app/controllers/thoughts_controller.rb
@@ -20,6 +20,24 @@ class ThoughtsController < ApplicationController
 
   def edit
   end
+  
+  def thoughtstream
+    raw_users = User.find_user(params[:user_id])
+    raw_users.present? ? @user = raw_users.first.first : @user = nil
+
+    raw_thoughtstream = @user.thoughtstream(10)
+    # Remember that this comes in the form of (subscription, thought)
+    # TODO: Currently no worky
+    @thoughtstream = raw_thoughtstream.collect{ |ts| ts[1] }
+  end
+  
+  def thoughts
+    raw_users = User.find_user(params[:user_id])
+    raw_users.present? ? @user = raw_users.first.first : @user = nil
+
+    raw_thoughts = @user.my_thoughts(10)
+    @thoughts = raw_thoughts.collect{ |t| t.first }
+  end
 
   # def show
   #   @owner = User.find_user(params[:user_id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :users do |user|
-    user.resources :thoughts
+    user.resources :thoughts, :collection => { :thoughts => :get, :thoughtstream => :get }
     user.resources :subscriptions
   end
 
